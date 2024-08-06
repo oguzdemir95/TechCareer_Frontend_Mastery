e3de7540e5593280efbeaf56ff549fdb075686e1
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-08-06 22:58:13 +0300

    Operator, Prompt, Math

--- _5_JavaScript/template.js
+++ _5_JavaScript/template.js
@@ -15,26 +15,79 @@
 
 //////////////////////////////////////////////
 // Variable (Global)
-var kelime="Js Tutorial";
-console.log(kelime);
-// alert(kelime)
+// var kelime="Js Tutorial";
+// console.log(kelime);
+// // alert(kelime)
 
-// Hoisting
-kelime44="Kelimeler";
-var kelime44;
+// // Hoisting
+// kelime44="Kelimeler";
+// var kelime44;
 
-var sayi=44;
-console.log(sayi);
-var sayi=44.23;
-console.log(sayi);
+// var sayi=44;
+// console.log(sayi);
+// var sayi=44.23;
+// console.log(sayi);
 
-var karar=true;
-console.log(karar);
+// var karar=true;
+// console.log(karar);
 
-// Let
-let kelime2="Math"
-console.log(kelime2);
+// // Let
+// let kelime2="Math"
+// console.log(kelime2);
 
-// Const
-const kelime4="Math"
-console.log(kelime4);
+// // Const
+// const kelime4="Math"
+// console.log(kelime4);
+
+// Operators
+// let number1=11;
+// let number2=2;
+// console.log(number1+number2);
+// console.log(number1-number2);
+// console.log(number1*number2);
+// console.log(number1/number2);
+// console.log(number1%number2);
+// console.log(number1**number2);
+
+// && Ve || Veya ! Değil > >= < <=
+// Atama
+// == Eşittir (Türüne bakmaksızın)
+// === Eşittir (Türüne bakarak)
+
+////////////////////////////////////////////////////
+// Kullanıcıdan veri alma
+// let user1=Number(prompt("Lütfen 1. sayıyı giriniz"));
+// let user2=parseInt(prompt("Lütfen 2. sayıyı giriniz"));
+
+// console.log(user1+user2);
+// console.log(user1-user2);
+// console.log(user1*user2);
+// console.log(user1/user2);
+// console.log(user1%user2);
+// console.log(user1**user2);
+//////////////////////////////////////////////////////////
+
+// Math
+let user=Number(prompt("Lütfen 1. sayıyı giriniz"));
+console.log("Pi: "+Math.PI);
+console.log("E: "+Math.E);
+console.log("Mutlak Değer: "+Math.abs(user));
+console.log("Karekök: "+Math.sqrt(user));
+console.log("Kare: "+Math.pow(user,2));
+
+console.log("Yuvarla: "+Math.round(7.3));
+console.log("Yuvarla: "+Math.round(7.5));
+console.log("Aşağı Yuvarla: "+Math.floor(8.9));
+console.log("Yukarı Yuvarla: "+Math.ceil(8.1));
+
+console.log("Rastgele: "+Math.random());
+console.log("Rastgele: "+Math.random()*4+1);
+console.log("Rastgele: "+Math.floor(Math.random()*4+1));
+
+
+console.log("Sin: "+Math.sin(45));
+
+
+
+
+
