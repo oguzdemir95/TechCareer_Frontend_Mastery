a2e0e887a9a9cdec442ea61110d5f21b7246cc8f
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-08-06 22:56:23 +0300

    Window, Document, Log, Warn, Info, Alert, Variable, Let, Const

---
+++ _5_JavaScript/template.js
@@ -0,0 +1,40 @@
+// alert("External JS")
+// window.alert("External JS")
+
+// Log
+// console.log("Log");
+
+// Warning
+// console.warn("Warning");
+
+// Info
+// console.info("Info");
+
+// Error
+// console.error("Error");
+
+//////////////////////////////////////////////
+// Variable (Global)
+var kelime="Js Tutorial";
+console.log(kelime);
+// alert(kelime)
+
+// Hoisting
+kelime44="Kelimeler";
+var kelime44;
+
+var sayi=44;
+console.log(sayi);
+var sayi=44.23;
+console.log(sayi);
+
+var karar=true;
+console.log(karar);
+
+// Let
+let kelime2="Math"
+console.log(kelime2);
+
+// Const
+const kelime4="Math"
+console.log(kelime4);
