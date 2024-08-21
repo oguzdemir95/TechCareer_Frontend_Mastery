40133d7ae6df45c635e6696d92e1a37e157dff93
oguzdemir95 <141906081+oguzdemir95@users.noreply.github.com>
2024-08-21 12:55:01 +0300

    Fade, Show, Hide, Animation, Delay

--- _7_jQuery/jQuery.js
+++ _7_jQuery/jQuery.js
@@ -9,21 +9,50 @@ $(document).ready(function () {
 
 // HTML
 $(document).ready(function () {
-  $("#html_button").click(function(){
+  $("#html_button").click(function () {
     $("#html_id p").html("<p><b><mark>Diğer Satır</mark></b></p>")
   });
 });
 
 // Text
 $(document).ready(function () {
-  $("#text_button").click(function(){
+  $("#text_button").click(function () {
     $("#html_id p").html("<p>Text Diğer Satır</p>")
   });
 });
 
 // Attribute
-$(document).ready(function(){
+$(document).ready(function () {
   $("#attr_button").click(function () {
-    $("#attr_id").attr("href","https://www.google.com")
+    $("#attr_id").attr("href", "https://www.google.com")
   })
 });
+
+// Effect
+$(document).ready(function () {
+  $("#effect_button").click(function () {
+    alert("Uyarı")
+    $("#effect_id").animate({ left: '250px' });
+  });
+});
+
+// Fade
+$(document).ready(function () {
+  $("#fadeout_id").click(function () {
+    $("#fade_parag_id").fadeOut()
+  });
+  $("#fadein_id").click(function () {
+    $("#fade_parag_id").fadeIn();
+  });
+});
+
+// Show - Hide
+$(document).ready(function () {
+  $("#hide_button").click(function () {
+    $("#effect_id").hide(1000);
+  });
+  $("#show_button").click(function () {
+    $("#effect_id").show(1000);
+  });
+});
+
