888625c98891cf8f6929790f951f6405f3951458
oguzhandemir-od <141906081+oguzhandemir-od@users.noreply.github.com>
2024-09-16 11:01:28 +0300

    Reusability

--- _8_ReactJS/blog/src/components/register/RegisterForm.jsx
+++ _8_ReactJS/blog/src/components/register/RegisterForm.jsx
@@ -1,41 +1,40 @@
 import React, { useEffect, useState } from "react";
 import { Form } from "react-router-dom";
+import ReusabilityTextInput from "./ReusabilityTextInput";
 
-
-const RegisterForm=()=> {
+const RegisterForm = () => {
   // State
   // Form State
   const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
 
   // Loading State
-  const [loading,setLoading]=useState(false);
+  const [loading, setLoading] = useState(false);
 
   // Multiple Request State
-  const [multipleRequest,setMultipleRequest]=useState(false);
+  const [multipleRequest, setMultipleRequest] = useState(false);
 
   // Error
   const [errors, setErrors] = useState({});
 
   // isFormValid
-  const [isFormValid,setIsFormValid]=useState(false);
+  const [isFormValid, setIsFormValid] = useState(false);
 
   ////////////////////////////////////////////////
 
   // Form Data hepsi doluysa
-  useEffect(()=>{
-    const{username,email,password,confirmPassword}=formData;
-    if(username&&email&&password&&confirmPassword){
+  useEffect(() => {
+    const { username, email, password, confirmPassword } = formData;
+    if (username && email && password && confirmPassword) {
       setIsFormValid(true);
+    } else {
+      setIsFormValid(false);
     }
-    else{
-      setIsFormValid(false)
-    }
-  },[formData]);
+  }, [formData]);
 
   // Handle
   const handleChange = (e) => {
@@ -54,7 +53,7 @@ const RegisterForm=()=> {
 
     // Username
     if (formData.username.trim().length < 3) {
-      tempErrors.username = 'Kullanıcı adı için az 3 karakter girmelisiniz.';
+      tempErrors.username = "Kullanıcı adı için az 3 karakter girmelisiniz.";
       isValid = false;
     }
 
@@ -82,7 +81,7 @@ const RegisterForm=()=> {
   };
 
   // Submit
-  let handleSubmit = async(e) => {
+  let handleSubmit = async (e) => {
     // Browser dursun, bir şey yapmasın
     e.preventDefault();
 
@@ -109,39 +108,40 @@ const RegisterForm=()=> {
       <h1 className="text-center display-4 text-primary text-uppercase mb-4 mt-4">
         Register
       </h1>
-      <form onSubmit={handleSubmit} className='mb-5'>
+      <form onSubmit={handleSubmit} className="mb-5">
         {/* Username */}
         <div className="form-group mb-4">
           <label>Kullanıcı Adı:</label>
-          <input type="text" 
-          name="username" 
-          
-          className="form-control" 
-          onChange={handleChange}
-          value={formData.username}/>
+          <input
+            type="text"
+            name="username"
+            className="form-control"
+            onChange={handleChange}
+            value={formData.username}
+          />
           {/* {errors.username && <p style={{color:'red'}}>{errors.username}</p>} */}
 
-          {errors.username && <div
-            class="alert alert-info alert-dismissible fade show mt-2"
-            role="alert"
-          >
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="alert"
-              aria-label="Close"
-            ></button>
-          
-            <strong>Validation</strong> {errors.username && <p style={{color:'blue'}}>{errors.username}</p>}
-          </div>}
-
-
-          
-          
+          {errors.username && (
+            <div
+              class="alert alert-info alert-dismissible fade show mt-2"
+              role="alert"
+            >
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="alert"
+                aria-label="Close"
+              ></button>
+              <strong>Validation</strong>{" "}
+              {errors.username && (
+                <p style={{ color: "blue" }}>{errors.username}</p>
+              )}
+            </div>
+          )}
         </div>
 
         {/* Email */}
-        <div className="form-group mb-4">
+        {/* <div className="form-group mb-4">
           <label>Email:</label>
           <input type="email" 
           name="email" 
@@ -150,10 +150,20 @@ const RegisterForm=()=> {
           onChange={handleChange}
           value={formData.email}/>
           {errors.email && <p style={{color:'red'}}>{errors.email}</p>}
-        </div>
+        </div> */}
+
+        <ReusabilityTextInput
+          label="Email"
+          type="email"
+          name="email"
+          className="form-control"
+          value={formData.email}
+          onChange={handleChange}
+          error={errors.email}
+        />
 
         {/* Password */}
-        <div className="form-group mb-4">
+        {/* <div className="form-group mb-4">
           <label>Şifre:</label>
           <input type="password" 
           name="password" 
@@ -162,10 +172,20 @@ const RegisterForm=()=> {
           onChange={handleChange}
           value={formData.password}/>
           {errors.password && <p style={{color:'red'}}>{errors.password}</p>}
-        </div>
+        </div> */}
+
+        <ReusabilityTextInput
+          label="Şifre"
+          type="password"
+          name="password"
+          className="form-control"
+          value={formData.password}
+          onChange={handleChange}
+          error={errors.password}
+        />
 
         {/* Re-Password */}
-        <div className="form-group mb-4">
+        {/* <div className="form-group mb-4">
           <label>Şifre Tekrarı:</label>
           <input type="password" 
           name="confirmPassword" 
@@ -174,29 +194,43 @@ const RegisterForm=()=> {
           onChange={handleChange}
           value={formData.confirmPassword}/>
           {errors.confirmPassword && <p style={{color:'red'}}>{errors.confirmPassword}</p>}
-        </div>
+        </div> */}
 
-        <button type="submit" disabled={!isFormValid} className="btn btn-primary me-3">
-        {(loading)&&<div
-            class="d-flex justify-content-center align-items-center"
-          >
-            <div
-              class="spinner-border text-secondary spinner-border-sm"
-              role="status"
-            >
-              
-              <span class="visually-hidden">Loading...</span>
+        <ReusabilityTextInput
+          label="Şifre Tekrarı"
+          type="password"
+          name="confirmPassword"
+          className="form-control"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+          error={errors.confirmPassword}
+        />
+
+        {/* Submit */}
+        <button
+          type="submit"
+          disabled={!isFormValid}
+          className="btn btn-primary me-3"
+        >
+          {loading && (
+            <div class="d-flex justify-content-center align-items-center">
+              <div
+                class="spinner-border text-secondary spinner-border-sm"
+                role="status"
+              >
+                <span class="visually-hidden">Loading...</span>
+              </div>
             </div>
-          </div>}
-          
-          
-          Kayıt Ol</button>
-        <button type="reset" className="btn btn-danger">Temizle</button>
-
+          )}
+          Kayıt Ol
+        </button>
+        <button type="reset" className="btn btn-danger">
+          Temizle
+        </button>
       </form>
     </>
   );
-}
+};
 
 // Export
 export default RegisterForm;
